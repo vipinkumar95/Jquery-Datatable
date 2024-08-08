f95c742f3c0d2db9ba1a156c692bbf4e16ef8a8a
Rudra Pal <Rudra Pal@DESKTOP-EPCRK2D>
2024-08-08 17:46:27 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using JqueryDatatable.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryDatatable.Controllers
+{
+    public class HomeController : Controller
+    {
+        JquerdataEntities1 db = new JquerdataEntities1();
+        public ActionResult Index()
+        {
+            var data  = db.loadings.ToList();
+            return View(data);
+        }
+
+       
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JqueryDatatable.MvcApplication" Language="C#" %>
